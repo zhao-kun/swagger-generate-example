07eb2bf80a7f33c2758874ee99590b0685503a22
Stel000 <kkcyc1996@gmail.com>
2019-09-26 18:24:48 +0800

    [Imp]Update dependency of swagger-maven-plugin for source install

---
+++ src/main/java/stress/Hello.java
@@ -0,0 +1,12 @@
+package stress;
+
+import lombok.Data;
+
+@Data
+public class Hello {
+    private String hello;
+
+    private String name;
+
+    private Integer age;
+}
--- src/main/java/stress/Main.java
+++ src/main/java/stress/Main.java
@@ -1,12 +1,16 @@
 package stress;
 
-import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -45,6 +49,12 @@ public class Main {
 	public String sleep200() {
 		this.sleep(200);
 		return "Sleep 200";
+	}
+
+	@GetMapping("sayHello/{one}")
+	@ApiResponse(code = 400,message = "parameter error")
+	public ResponseEntity<Hello> sayHello(@PathVariable("one") @RequestParam("hello") String one) {
+		return ResponseEntity.ok(new Hello());
 	}
 
 }
